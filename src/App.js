ab818ea37aa8a2598bc764f4efcdf88d984dd002
jc <avaz28082000@gmail.com>
2020-09-06 19:50:47 +0500

    Changed default vals

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './app.scss';
 const App = () => {
     return (
         <div className="container">
-            <RangeSlider min={0} max={80} defaultMin={6} defaultMax={15} />
+            <RangeSlider min={0} max={20} defaultMin={6} defaultMax={15} />
         </div>
     );
 };
